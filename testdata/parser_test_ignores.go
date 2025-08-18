8085077301dca9cdcad6865b78a9523618dc6b1c
godcong <jumbycc@163.com>
2025-08-18 19:23:36 +0800

    refactor(adptool): update parser test files for global directives
    - Update parser_test_functions.go to test global function directives
    - Update parser_test_ignores.go to test global ignore directives
    - Update parser_test_variables.go to test global variable directives
    - Simplify ignore directive formats in parser_test_ignores.go

--- testdata/parser_test_ignores.go
+++ testdata/parser_test_ignores.go
@@ -1,11 +1,7 @@
 package testdata
 
+// Test global ignore directives
 //go:adapter:ignore pattern1
-//go:adapter:ignores pattern2,pattern3
-//go:adapter:ignores:json ["pattern4", "pattern5"]
-
-// This file is used to test the parsing of ignore and ignores directives.
-// It contains various formats of ignore directives:
-// 1. Single pattern using ignore
-// 2. Multiple patterns using ignores with comma separation
-// 3. Multiple patterns using ignores:json with JSON array format
+//go:adapter:ignores pattern2 pattern3
+//go:adapter:ignore pattern4
+//go:adapter:ignores pattern5 pattern6 pattern7
